5e54ba48c0830b518230d7b4c115ced95535f37a
路博 <lubo@lubos-MacBook-Pro.local>
2020-06-06 23:05:59 +0800

    lesson2

--- lesson2/substrate-node-template/pallets/template/src/lib.rs
+++ lesson2/substrate-node-template/pallets/template/src/lib.rs
@@ -9,8 +9,10 @@
 /// For more guidance on Substrate FRAME, see the example pallet
 /// https://github.com/paritytech/substrate/blob/master/frame/example/src/lib.rs
 
-use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch};
+use frame_support::{decl_module, decl_storage, decl_event, decl_error, ensure, StorageMap};
 use frame_system::{self as system, ensure_signed};
+use sp_core::H256;
+use sp_std::vec::Vec;
 
 #[cfg(test)]
 mod mock;
@@ -32,30 +34,26 @@ decl_storage! {
 	// storage items are isolated from other pallets.
 	// ---------------------------------vvvvvvvvvvvvvv
 	trait Store for Module<T: Trait> as TemplateModule {
-		// Just a dummy storage item.
-		// Here we are declaring a StorageValue, `Something` as a Option<u32>
-		// `get(fn something)` is the default getter which returns either the stored `u32` or `None` if nothing stored
-		Something get(fn something): Option<u32>;
+        Proofs: map hasher(blake2_128_concat) H256 => (T::AccountId, T::BlockNumber);
 	}
 }
 
 // The pallet's events
 decl_event!(
 	pub enum Event<T> where AccountId = <T as system::Trait>::AccountId {
-		/// Just a dummy event.
-		/// Event `Something` is declared with a parameter of the type `u32` and `AccountId`
-		/// To emit this event, we call the deposit function, from our runtime functions
-		SomethingStored(u32, AccountId),
+		ClaimCreated(AccountId, H256),
+		ClaimRevoked(AccountId, H256),
+		ClaimTransfer(AccountId, AccountId, H256),
 	}
 );
 
 // The pallet's errors
 decl_error! {
 	pub enum Error for Module<T: Trait> {
-		/// Value was None
-		NoneValue,
-		/// Value reached maximum and cannot be incremented further
-		StorageOverflow,
+		ProofAlreadyClaimed,
+		NoSuchProof,
+		NotProofOwner,
+		ProofOutOfbound,
 	}
 }
 
@@ -72,38 +70,54 @@ decl_module! {
 		// this is needed only if you are using events in your pallet
 		fn deposit_event() = default;
 
-		/// Just a dummy entry point.
-		/// function that can be called by the external world as an extrinsics call
-		/// takes a parameter of the type `AccountId`, stores it, and emits an event
-		#[weight = 10_000]
-		pub fn do_something(origin, something: u32) -> dispatch::DispatchResult {
-			// Check it was signed and get the signer. See also: ensure_root and ensure_none
-			let who = ensure_signed(origin)?;
+        #[weight = 0]
+		fn create_claim(origin, proof: H256){
+			
+			let sender = ensure_signed(origin)?;
+			
+			ensure!(!Proofs::<T>::contains_key(&proof), Error::<T>::ProofAlreadyClaimed);
 
-			// Code to execute when something calls this.
-			// For example: the following line stores the passed in u32 in the storage
-			Something::put(something);
+			let current_block = <system::Module<T>>::block_number();
 
-			// Here we are raising the Something event
-			Self::deposit_event(RawEvent::SomethingStored(something, who));
-			Ok(())
+			Proofs::<T>::insert(&proof, (&sender, current_block));
+
+			Self::deposit_event(RawEvent::ClaimCreated(sender, proof));
+
+		}
+        #[weight = 0 ]
+		fn revoke_claim(origin, proof: H256){
+			
+			let sender = ensure_signed(origin)?;
+
+			ensure!(Proofs::<T>::contains_key(&proof), Error::<T>::NoSuchProof);
+
+			let (owner, _) = Proofs::<T>::get(&proof);
+
+			ensure!(owner == sender, Error::<T>::NotProofOwner);
+
+			Proofs::<T>::remove(&proof);
+
+			Self::deposit_event(RawEvent::ClaimRevoked(sender,proof));
+
 		}
 
-		/// Another dummy entry point.
-		/// takes no parameters, attempts to increment storage value, and possibly throws an error
-		#[weight = 10_000]
-		pub fn cause_error(origin) -> dispatch::DispatchResult {
-			// Check it was signed and get the signer. See also: ensure_root and ensure_none
-			let _who = ensure_signed(origin)?;
+		#[weight = 0]
+		fn transfer_claim(origin, proof:H256, receiver: T::AccountId){
+			
+			let sender = ensure_signed(origin)?;
 
-			match Something::get() {
-				None => Err(Error::<T>::NoneValue)?,
-				Some(old) => {
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					Something::put(new);
-					Ok(())
-				},
-			}
+			ensure!(Proofs::<T>::contains_key(&proof), Error::<T>::NoSuchProof);
+
+			let (owner, _) = Proofs::<T>::get(&proof);
+
+			ensure!(owner == sender, Error::<T>::NotProofOwner);
+			
+			Proofs::<T>::remove(&proof);
+
+			let current_block = <system::Module<T>>::block_number();
+			Proofs::<T>::insert(&proof, (&receiver, current_block));
+
+			Self::deposit_event(RawEvent::ClaimTransfer(sender, receiver, proof));
 		}
 	}
 }
